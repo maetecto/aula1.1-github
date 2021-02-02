abcc968479b2aae4c1a679be1eb3b50540df0b13
Artur Matos <artur_paixao_de_matos@hotmail.com>
2021-02-02 13:54:22 +0000

    teste de save

--- src/MatrizEx.java
+++ src/MatrizEx.java
@@ -83,19 +83,19 @@ public class Program {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		int m = sc.nextInt();
 		int n = sc.nextInt();
 		int[][] mat = new int[m][n];
-		
+
 		for (int i=0; i<mat.length; i++) {
 			for (int j=0; j<mat[i].length; j++) {
 				mat[i][j] = sc.nextInt();
 			}
 		}
-		
+
 		int x = sc.nextInt();
-		
+
 		for (int i=0; i<mat.length; i++) {
 			for (int j=0; j<mat[i].length; j++) {
 				if  (mat[i][j] == x) {
@@ -115,7 +115,7 @@ public class Program {
 				}
 			}
 		}
-		
+
 		sc.close();
 	}
 }
